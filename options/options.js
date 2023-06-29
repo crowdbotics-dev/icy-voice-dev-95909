caa7821424e38733e5d818b73a1e00ea96d9eee5
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 10:32:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-voice-dev-95909",
+  url: "https://icy-voice-dev-95909.botics.co",
+  api: "https://icy-voice-dev-95909.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
